e53ebd0dcf76379ada2cc79297911f378976bfc0
nicung <nicung@student.ubc.ca>
2025-03-06 22:18:50 -0800

    inverted screen

--- src/gui.h
+++ src/gui.h
@@ -18,45 +18,6 @@ static lv_style_t style_font_12;
 static lv_style_t style_font_16;
 static lv_style_t style_font_14;
 
-// extern lv_color_t intervalMaxColour;
-// extern lv_color_t intervalMinColour;
-
-// extern int smallFontOffset;
-// extern int bigFontOffset;
-// extern float currentAng;
-// extern lv_obj_t *readout;
-// extern lv_color_t UIColour;
-// extern lv_obj_t *units;
-// extern char * unitLabel;
-// extern lv_color_t backgroundColour;
-// extern lv_obj_t *screen;
-// extern bool intervalTicks;
-
-// extern int tickOffset;
-// extern float maxVal;
-// extern float minVal;
-// extern float intervalMax;
-// extern float intervalMin;
-
-// extern int decimals;
-// extern int numTicks; 
-// extern int totalSmallTicks;
-// extern int numBoldTicks;
-// extern int boldTicksLength;
-// extern int tickLength;
-// extern int tickLabelOffset;
-// extern std::vector<lv_obj_t*> bigTicks; 
-// extern std::vector<lv_obj_t*> smallTicks;
-// extern std::vector<lv_obj_t*> bigTickLabels;
-// extern std::vector<lv_obj_t*> smallTickLabels;
-// extern std::vector<std::array<lv_point_precise_t, 2>> bigTicksCoords;
-// extern std::vector<std::array<lv_point_precise_t, 2>> smallTicksCoords;
-// extern std::vector<std::string> bigTickStringLabels;
-// extern std::vector<std::string> smallTickStringLabels;
-// extern lv_obj_t* intervalMaxTick;
-// extern lv_obj_t* intervalMinTick;
-// extern int intervalTickLength; 
-
 void drawUI();
 void bootAnimation();
 void guiInit();
